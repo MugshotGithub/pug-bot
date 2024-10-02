73bdce7e20e61c87d66ced30771f4f1c59c1b10a
Ollie Geenty <oligeenty@gmail.com>
2024-10-03 05:55:17 +1300

    Migrate data store to DB

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 import os
+import sqlite3
 
 import discord
 from discord import app_commands
@@ -9,10 +10,6 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
-bot = discord.Client(intents=intents)
-tree = app_commands.CommandTree(bot)
-guildId = int(os.getenv("GUILD_ID"))
-
 # Get env values, quit if incorrectly configured
 if os.path.exists(".env"):
     config = dotenv_values(".env")
@@ -21,6 +18,11 @@ else:
     quit(1)
 
 
+bot = discord.Client(intents=intents)
+tree = app_commands.CommandTree(bot)
+guildId = int(config["GUILD_ID"])
+
+@bot.event
 async def on_ready():
     await tree.sync(guild=discord.Object(id=guildId))
     print(f'Logged in as {bot.user.name}')
@@ -28,27 +30,72 @@ async def on_ready():
 
 
 async def _isAdmin(userId):
-    adminFile = open("admins.json")
-    adminData = json.load(adminFile)
-    adminFile.close()
+
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT * FROM admins WHERE id = (?) ;', (userId,))
+    memberFound = cursor.fetchall()
+
+    cursor.execute('SELECT * FROM admins WHERE type = "role"')
+    roleIds = cursor.fetchall()
+
+    conn.close()
+
+    if len(memberFound) > 1:
+        return True
 
     guild = bot.get_guild(guildId) if bot.get_guild(guildId) is not None else await bot.fetch_guild(guildId)
     member = guild.get_member(userId) if guild.get_member(userId) is not None else await guild.fetch_member(userId)
 
     if member.guild_permissions.administrator:
         return True
+    print(roleIds)
+    memberRoleIds = [role.id for role in member.roles]
 
-    for adminId in adminData["users"]:
-        if userId == adminId:
-            return True
-
-    roleIds = [role.id for role in member.roles]
-
-    for roleId in adminData["roles"]:
-        if roleId in roleIds:
+    for memberRoleId in memberRoleIds:
+        if memberRoleId in roleIds:
             return True
 
     return False
+
+@tree.command(name="setup-pugs", description="Initialised all channels for PUG lobbies",guild=discord.Object(id=guildId))
+async def setup_pug(interaction):
+    await interaction.response.defer(ephemeral=True)
+    if not await _isAdmin(interaction.user.id):
+        await interaction.followup.send(f"You do not have permission to use this command")
+        return
+    guild = bot.get_guild(guildId) if bot.get_guild(guildId) is not None else await bot.fetch_guild(guildId)
+
+    category = await guild.create_category("pugs")
+
+    signUpChannel = await category.create_text_channel("sign-up")
+
+    await signUpChannel.set_permissions(guild.default_role, read_messages=True, send_messages=False)
+    await signUpChannel.set_permissions(guild.get_member(bot.user.id), read_messages=True, send_messages=False)
+
+    discussionChannel = await category.create_text_channel("pug-discussion")
+
+    if not os.path.exists("lobbyData.json"):
+        with open("lobbyData.json.txt", "w") as file:
+            file.write("{}")
+
+    # TODO: Send sign up message HERE
+
+    lobbyChannel = await category.create_voice_channel("Lobby")
+    redTeamChannel = await category.create_voice_channel("Red Team")
+    blueTeamChannel = await category.create_voice_channel("Blue Team")
+    channelIds = {
+        "signUp": signUpChannel.id,
+        "discussion": discussionChannel.id,
+        "lobby": lobbyChannel.id,
+        "red": redTeamChannel.id,
+        "blue": blueTeamChannel.id
+    }
+
+
+
+
 
 #########################################################
 # This bot uses an internal admin system. Users with Admin permissions from the server are also granted these permissions
@@ -61,22 +108,12 @@ async def add_admin_user(interaction, member: discord.Member):
         await interaction.followup.send(f"You do not have permission to use this command")
         return
 
-    if os.path.exists("lobbyData.json"):
-        guild = bot.get_guild(guildId) if bot.get_guild(guildId) is not None else await bot.fetch_guild(guildId)
-        with open("lobbyData.json") as data:
-            jsonData = json.load(data)
-            channel = guild.get_channel(jsonData["auditId"]) if guild.get_channel(jsonData["auditId"]) is not None else await guild.fetch_channel(jsonData["auditId"])
-            await channel.set_permissions(member, read_messages=True)
+    conn = sqlite3.connect('database.db', autocommit=True)
+    cursor = conn.cursor()
 
-    adminFile = open("admins.json")
-    adminData = json.load(adminFile)
+    cursor.execute('INSERT OR IGNORE INTO admins (id,type) VALUES (?,"user");', (member.id,))
 
-    adminData["users"].append(member.id)
-    adminFile.close()
-
-    adminFile = open("admins.json", "w")
-    json.dump(adminData, adminFile)
-    adminFile.close()
+    conn.close()
 
     await interaction.followup.send(f"Added {member.display_name} to the admin list")
 
@@ -89,25 +126,12 @@ async def remove_admin_user(interaction, member: discord.Member):
         await interaction.followup.send(f"You do not have permission to use this command")
         return
 
-    if os.path.exists("lobbyData.json"):
-        guild = bot.get_guild(guildId) if bot.get_guild(guildId) is not None else await bot.fetch_guild(guildId)
-        with open("lobbyData.json") as data:
-            jsonData = json.load(data)
-            channel = guild.get_channel(jsonData["auditId"]) if guild.get_channel(jsonData["auditId"]) is not None else await guild.fetch_channel(jsonData["auditId"])
-            await channel.set_permissions(member, read_messages=False)
+    conn = sqlite3.connect('database.db', autocommit=True)
+    cursor = conn.cursor()
 
-    adminFile = open("admins.json")
-    adminData = json.load(adminFile)
+    cursor.execute('DELETE FROM admins WHERE id = ?;', (member.id,))
 
-    try:
-        adminData["users"].remove(member.id)
-    except ValueError:
-        pass
-
-    adminFile.close()
-    adminFile = open("admins.json", "w")
-    json.dump(adminData, adminFile)
-    adminFile.close()
+    conn.close()
 
     await interaction.followup.send(f"Removed {member.display_name} from the admin list")
 
@@ -120,22 +144,12 @@ async def add_admin_role(interaction, role: discord.Role):
         await interaction.followup.send(f"You do not have permission to use this command")
         return
 
-    if os.path.exists("lobbyData.json"):
-        guild = bot.get_guild(guildId) if bot.get_guild(guildId) is not None else await bot.fetch_guild(guildId)
-        with open("lobbyData.json") as data:
-            jsonData = json.load(data)
-            channel = guild.get_channel(jsonData["auditId"]) if guild.get_channel(jsonData["auditId"]) is not None else await guild.fetch_channel(jsonData["auditId"])
-            await channel.set_permissions(role, read_messages=True)
+    conn = sqlite3.connect('database.db', autocommit=True)
+    cursor = conn.cursor()
 
-    adminFile = open("admins.json")
-    adminData = json.load(adminFile)
+    cursor.execute('INSERT OR IGNORE INTO admins (id,type) VALUES (?,"role");', (role.id,))
 
-    adminData["roles"].append(role.id)
-
-    adminFile.close()
-    adminFile = open("admins.json", "w")
-    json.dump(adminData, adminFile)
-    adminFile.close()
+    conn.close()
 
     await interaction.followup.send(f"Added {role.name} to the admin list")
 
@@ -148,26 +162,81 @@ async def remove_admin_role(interaction, role: discord.Role):
         await interaction.followup.send(f"You do not have permission to use this command")
         return
 
-    if os.path.exists("lobbyData.json"):
-        guild = bot.get_guild(guildId) if bot.get_guild(guildId) is not None else await bot.fetch_guild(guildId)
-        with open("lobbyData.json") as data:
-            jsonData = json.load(data)
-            channel = guild.get_channel(jsonData["auditId"]) if guild.get_channel(jsonData["auditId"]) is not None else await guild.fetch_channel(jsonData["auditId"])
-            await channel.set_permissions(role, read_messages=False)
+    conn = sqlite3.connect('database.db', autocommit=True)
+    cursor = conn.cursor()
 
-    adminFile = open("admins.json")
-    adminData = json.load(adminFile)
+    cursor.execute('DELETE FROM admins WHERE id = ?;', (role.id,))
 
-    try:
-        adminData["roles"].remove(role.id)
-    except ValueError:
-        pass
-
-    adminFile.close()
-    adminFile = open("admins.json", "w")
-    json.dump(adminData, adminFile)
-    adminFile.close()
+    conn.close()
 
     await interaction.followup.send(f"Removed {role.name} from the admin list")
+
+@tree.command(name="clear-admin-list", description="Clears entire admin list",
+              guild=discord.Object(id=guildId))
+async def clear_admin_list(interaction):
+    await interaction.response.defer(ephemeral=True)
+    if not await _isAdmin(interaction.user.id):
+        await interaction.followup.send(f"You do not have permission to use this command")
+        return
+
+    conn = sqlite3.connect('database.db', autocommit=True)
+    cursor = conn.cursor()
+
+    cursor.execute('DELETE FROM admins;')
+
+    conn.close()
+
+    await interaction.followup.send(f"Cleared the admin list")
+
+@tree.command(name="list-admin-users", description="Lists current admin users",
+              guild=discord.Object(id=guildId))
+async def see_admin_users(interaction):
+    await interaction.response.defer(ephemeral=True)
+    if not await _isAdmin(interaction.user.id):
+        await interaction.followup.send(f"You do not have permission to use this command")
+        return
+
+    guild = bot.get_guild(guildId) if bot.get_guild(guildId) is not None else await bot.fetch_guild(guildId)
+
+    conn = sqlite3.connect('database.db', autocommit=True)
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT id FROM admins WHERE type == "user";')
+    memberIds = cursor.fetchall()
+    conn.close()
+
+    members = "Admin members:"
+    for memberId in memberIds:
+        memberId = memberId[0]
+        member = guild.get_member(memberId) if guild.get_member(memberId) is not None else await guild.fetch_member(memberId)
+        members += f"\n{member.name}"
+
+    await interaction.followup.send(members)
+
+@tree.command(name="list-admin-roles", description="Lists roles on the admin list",
+              guild=discord.Object(id=guildId))
+async def see_admin_roles(interaction):
+    await interaction.response.defer(ephemeral=True)
+    if not await _isAdmin(interaction.user.id):
+        await interaction.followup.send(f"You do not have permission to use this command")
+        return
+
+    guild = bot.get_guild(guildId) if bot.get_guild(guildId) is not None else await bot.fetch_guild(guildId)
+
+    conn = sqlite3.connect('database.db', autocommit=True)
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT id FROM admins WHERE type == "role";')
+    roleIds = cursor.fetchall()
+
+    conn.close()
+
+    roles = "Admin roles:"
+    for roleId in roleIds:
+        roleId = roleId[0]
+        role = guild.get_role(roleId)
+        roles += f"\n{role.name}"
+
+    await interaction.followup.send(roles)
 
 bot.run(config["BOT_KEY"])
